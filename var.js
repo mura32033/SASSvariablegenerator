69b78a6ece0514c95c3560ff52805aff7f869c19
mura32033 <mura3-2033@hotmail.co.jp>
2020-12-12 16:36:37 +0900

    added class generator

--- var.js
+++ var.js
@@ -2,21 +2,55 @@
 
 var input = document.getElementById('input');
 var outputText = document.getElementById('output');
+var varName = document.getElementById('varname');
+var classname = document.getElementById('classname');
+var outputNest = document.getElementById('outputnest');
+
+var check = document.getElementById('classcheck');
+var generateClass = document.getElementById('generateclass');
+var outputClass = document.getElementById('outputclass');
+check.addEventListener("change", function(){
+  if(check.checked != true){
+  generateClass.style.display = 'none';
+  outputClass.style.display = 'none';
+} else {
+  generateClass.style.display = 'block';
+  outputClass.style.display = 'flex';
+}
+})
+
 
 function output(){
-  var name = document.getElementById('varname');
   var inputArray = [];
   var outputArray = [];
   inputArray = (input.value.split('\n'));
   inputArray.forEach(function(i, index){
     index = (index + 1) * 10;
-    var line = '$' + name.value + index + ': '+ i + ';';
+    var line = '$' + varName.value + index + ': '+ i + ';';
+    
     outputArray.push(line);
   });
+
+  if(check.checked){
+    var nest = `$lists: `+ inputArray +`;
+@each $var in $lists {
+  $key: index($lists, $var) * 10;
+  .`+ classname.value +`#{$key}{
+    background-color: $var;
+  }
+}`;
+    outputNest.value = nest;;
+  }
+
   outputText.value = outputArray.join('\n');
 }
 
-function copy(){
-  outputText.select();
+function copy(a){
+  if(a == 'output'){
+    outputText.select();
+  } else if (a == 'nest'){
+    outputNest.select()
+  }
+  
   document.execCommand('copy');
 }
